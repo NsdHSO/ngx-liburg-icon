7dd9833f4c825b8403054ff1b78e0e0374abaa94
(Nechiforel David-Samuel) NsdHSO <nechiforelsamuel@yahoo.com>
2022-08-08 16:41:58 +0300

    refactor: remove comonent

--- src/public-api.ts
+++ src/public-api.ts
@@ -7,4 +7,3 @@
 // export * from './lib/ngx-liburg-icon.module';
 export * from './lib/icon-core/icon-core.service'
 export * from './lib/icon-core/icon-core.module'
-export * from './lib/icon-core/page-not-found/page-not-found.component'
