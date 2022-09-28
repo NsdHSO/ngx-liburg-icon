74430ae5264af6509ce8111422acd1ce23849c62
(Nechiforel David-Samuel) NsdHSO <nechiforelsamuel@yahoo.com>
2022-09-28 12:03:15 +0300

    feat: added service for trigger

--- src/lib/spinner/spinner.component.ts
+++ src/lib/spinner/spinner.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {SpinnerService} from "./util/service/spinner.service";
 
 @Component({
              selector   : 'lib-spinner',
@@ -6,4 +8,9 @@ import {Component} from '@angular/core';
              styleUrls  : ['./spinner.component.scss'],
            })
 export class SpinnerComponent {
+  stateSpinner : BehaviorSubject<boolean>;
+
+  constructor(private readonly _spinnerService : SpinnerService) {
+    this.stateSpinner = _spinnerService.isLoading;
+  }
 }
