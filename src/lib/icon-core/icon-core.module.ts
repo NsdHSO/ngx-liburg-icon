37b5f0aa0ad68d8e90fa5dc6726d5d4d09cf733c
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2023-02-11 23:54:38 +0200

    chore:

--- src/lib/icon-core/icon-core.module.ts
+++ src/lib/icon-core/icon-core.module.ts
@@ -1,9 +1,9 @@
-import {ErrorHandler, NgModule} from '@angular/core';
-import {MatIconRegistry} from '@angular/material/icon';
-import {DomSanitizer} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
-import {GlobalErrorHandleService} from "./icon-core.service";
+import { HttpClientModule } from '@angular/common/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { GlobalErrorHandleService } from "./icon-core.service";
 
 @NgModule({
   imports: [HttpClientModule, RouterModule],
@@ -17,7 +17,7 @@ export class IconCoreModule {
     this._matIconRegistry.addSvgIconSetInNamespace(
       'fa_brands',
       this._domSanitizer.bypassSecurityTrustResourceUrl(
-        'assets/icons/svgs/brands.svg'
+        './assets/brands.svg'
       )
     );
     this._matIconRegistry.addSvgIconSetInNamespace(
